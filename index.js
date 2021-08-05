5d5b176f2fcb0157dff0c3f61ca20bd6abbd41bb
Zechariah <zechariahyeo02@gmail.com>
2021-08-05 14:49:23 +0800

    add animations to first and second click

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const imgContainer = document.getElementsByClassName("img-container")[0];
 const cta = document.getElementsByClassName("cta")[0];
 
 let checkClick = false;
+let headerTop = {"initial": 0, "final":0};
 
 function openCloseMouth(delay){
     if (varun.ariaLabel === "left"){
@@ -42,10 +43,26 @@ varun.addEventListener("click", () => {
 });
 
 header.addEventListener("click", () => {
+    let id = null;
+    let pos = 0;
+    let step = 0;
     if (header.classList.contains("initial")){
         header.classList.remove("initial");
         document.getElementsByClassName("title")[0].innerHTML = "Hi i'm <span class=\"accent-color\">Varun</span>";
+        clearInterval(id);
         openCloseMouth(300);
+        id = setInterval(() => {
+            const titleBg = document.getElementsByClassName("title-bg")[0];
+            if (pos == 100){
+                clearInterval(id);
+                pos = 0;
+            } else {
+                pos++;
+                titleBg.style.width = pos + "%";
+            }
+        }, 8);
+        headerTop.initial = header.getBoundingClientRect().top;
+        
     } else {
         if (checkClick){
             if (description.classList.contains("initial")){
@@ -53,10 +70,36 @@ header.addEventListener("click", () => {
                 openCloseMouth(300);
             }
         } else {
-            header.style.margin = "auto auto 0";
+            header.style.visibility = "hidden";
+            setTimeout(()=>{
+                console.log("hi")
+            }, 1000);
             description.innerHTML = "<div class=\"card-content\"><p>I am:</p><ul><li>Very handsome</li><li>Very cool</li><li>Varun</li></ul></div>";
             description.classList.add("card", "text-color", "sans-serif", "card-color", "initial");
-            openCloseMouth(300);
+
+            header.style.margin = "auto auto 0";
+            headerTop.final = header.getBoundingClientRect().top;
+            pos = headerTop.initial - headerTop.final;
+            step = pos / 100;
+            clearInterval(id);
+            openCloseMouth(500);
+            let first = true
+            id = setInterval(() => {
+                if (pos > 0){
+                    if (first){
+                        header.style.visibility = "visible";
+                        first = false;
+                    } else {
+                        pos -= step;
+                        header.style.transform= "translateY("+ pos+"px)";
+                    }
+                } else {
+                    clearInterval(id);
+                    pos = 0;
+                }
+            } ,5);
+
+
             checkClick = true;
         }
     }
